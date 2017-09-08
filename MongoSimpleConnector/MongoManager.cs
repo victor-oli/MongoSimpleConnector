331c42bb6cdcb683efad4c7a4b2cc30dc4bf20c0
João Victor de Oliveira Espinosa <jvespinosa@stone.com.br>
2017-09-08 17:13:38 -0300

    Add project files.

---
+++ MongoSimpleConnector/MongoManager.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+
+namespace MongoSimpleConnector
+{
+    public class MongoManager
+    {
+        private static string mongoDbConnection;
+        private static string mongoDbName;
+        private static IMongoClient _client;
+        private static IMongoDatabase _db;
+
+        public static void Config(string mongoDbConnection, string mongoDbName)
+        {
+            MongoManager.mongoDbConnection = mongoDbConnection;
+            MongoManager.mongoDbName = mongoDbName;
+        }
+
+        public static IMongoCollection<T> open<T>(string collectionName) where T : class
+        {
+            _client = new MongoClient(mongoDbConnection);
+            _db = _client.GetDatabase(mongoDbName);
+
+            return _db.GetCollection<T>(collectionName);
+        }
+    }
+}
